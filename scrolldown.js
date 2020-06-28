e3d2f9af11dd534d7aa23d8c08596bf2b585bb46
Kyle Timmermans <47165102+kyletimmermans@users.noreply.github.com>
2020-06-28 13:04:16 -0400

    Update scrolldown.js

--- scrolldown.js
+++ scrolldown.js
@@ -1,1 +1,1 @@
-var x = document.evaluate('/html/body/div[4]/div/div[2]', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;""x.scrollTo(0, x.scrollHeight);
+var x = document.evaluate('/html/body/div[4]/div/div/div[2]', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue; x.scrollTo(0, x.scrollHeight);
